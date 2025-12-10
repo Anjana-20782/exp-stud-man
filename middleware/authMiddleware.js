b8b2d334b890e624e4fcc450b0fc9b1970effeed
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-10 13:20:17 +0530

    new authentication features added

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  // no token = not allowed
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;  // store user info
+    next();              // go to next function
+
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};

d98bb3543b9e0a9a28144ac50c63e2f955ddd21e
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-10 13:16:48 +0530

    new fearure

--- models/User.js
+++ models/User.js
@@ -5,4 +5,8 @@ const userSchema=new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true}
 },{timestamps:true});
+
+
+
+
 export default mongoose.model("User",userSchema)

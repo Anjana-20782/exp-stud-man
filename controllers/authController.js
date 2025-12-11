5e9808af88afa184d4a143cb5c87af66b32e2939
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-11 19:37:19 +0530

    new authentication features added

---
+++ controllers/authController.js
@@ -0,0 +1,43 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const exist = await User.findOne({ email });
+    if (exist) return res.status(400).json({ message: "User already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = new User({ username, email, password: hashed });
+    await user.save();
+
+    res.json({ message: "Registration successful" });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ message: "Incorrect password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ message: "Login successful", token });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};

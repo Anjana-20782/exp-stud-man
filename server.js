2fb170841e31ae414c58eb4ab7228290a9b4a215
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-15 17:01:49 +0530

    a  choose file added

--- server.js
+++ server.js
@@ -17,7 +17,10 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // auth routes

d2632cc9fd035dc28fabfa08ca1b76880a58c88b
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-15 15:14:47 +0530

    WIP: auth controller changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,6 @@ export const registerUser = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const user = await User.create({
       name: name.trim(),
       email: normEmail,
